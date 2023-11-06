0ddd0066dd3499dceebd44cc123161c72fa07db3
Aaron Hathaway <aaronhathaway1@hotmail.com>
2023-11-05 23:57:00 -0700

    Added Slide component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import Slide from '@/components/Slide'
 
 export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
 			<h1>Portfolio</h1>
+			<Slide />
 		</main>
 	)
 }
